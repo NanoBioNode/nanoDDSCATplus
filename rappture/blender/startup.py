826477132b75fac8c147d12c2d99804f46f58513
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-14 13:57:35 +0000

    Adding tarfile member sanitization to extractall()

--- rappture/blender/startup.py
+++ rappture/blender/startup.py
@@ -238,7 +238,26 @@ class ImportTAR(bpy.types.Operator, ImportHelper):
             with tarfile.open(path,'r') as infile:
                 subpaths = infile.getnames()
                 delpath = subpaths.pop(0)
-                infile.extractall()
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(infile)
 
         for item in subpaths:
             truepaths.append(os.path.realpath(item))
